5c79b985d1bb3bc608932e3b862df53b5cf83a4f
민제현 <alswpgus0@gmail.com>
2021-05-28 19:07:52 +0900

    feature_makeSignControllerAndRoute_#7:Added signout controller

--- routes/sign.js
+++ routes/sign.js
@@ -5,6 +5,6 @@ const checkTokenValid = require("../middlewares/checkTokenValid");
 
 router.post("/in", controller.signin);
 router.post("/up", controller.signup);
-// router.get("/out", checkTokenValid, controller.signout);
+router.get("/out", checkTokenValid, controller.signout);
 
 module.exports = router;
