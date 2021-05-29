97a244b78380b2af7fb0ab2e2b58c43cccfd433c
민제현 <alswpgus0@gmail.com>
2021-05-29 20:02:43 +0900

    feature_makeDrawingControllerAndRoute_#9:Added controller:drawingUpload

---
+++ controllers/drawing/drawingUpload.js
@@ -0,0 +1,23 @@
+const { Drawing } = require("../../models");
+console.log(Drawing);
+
+module.exports = async (req, res) => {
+  // console.log(req.file);
+  // console.log(req.body);
+  // console.log(res.userId);
+  const DrawingImg = req.file.filename;
+  const Users_id = res.userId;
+  const { title, description, isCompleted } = req.body;
+  try {
+    await Drawing.create({
+      title,
+      DrawingImg,
+      description,
+      isCompleted,
+      Users_id,
+    });
+    res.status(200).send({ message: "Drawing post uploaded successful" });
+  } catch (err) {
+    res.status(500).send({ message: `${err.message}` });
+  }
+};
--- routes/drawing.js
+++ routes/drawing.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/drawing");
 const getImage = require("../middlewares/getImage");
 const checkTokenValid = require("../middlewares/checkTokenValid");
 
-router.post("/upload", controller.drawingUpload);
+router.post("/upload", checkTokenValid, getImage, controller.drawingUpload);
 router.patch("/update", controller.drawingUpdate);
 router.delete("/delete", controller.drawingDelete);
 
