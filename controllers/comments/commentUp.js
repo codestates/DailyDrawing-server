ee57ea6e0978b078638f0ce2bcc63b26985f5a47
민제현 <alswpgus0@gmail.com>
2021-05-30 21:01:05 +0900

    feature_makeCommentsControllerAndRoute_#13:Added CommentUp Controller function

--- controllers/comments/commentUp.js
+++ controllers/comments/commentUp.js
@@ -1,5 +1,13 @@
 const { Comments } = require("../../models");
 
 module.exports = async (req, res) => {
-  res.send("hello");
+  const Users_id = res.userId;
+  const { Drawings_id, comment } = req.body;
+
+  try {
+    Comments.create({ Users_id, Drawings_id, comment });
+    res.status(201).send({ message: "Your comment uploaded successfully" });
+  } catch (err) {
+    res.status(500).send({ message: `${err.message}` });
+  }
 };
