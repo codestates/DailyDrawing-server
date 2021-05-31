a6fdfde27dda193c0e5c36860cb82ec85ac9160b
민제현 <alswpgus0@gmail.com>
2021-05-31 20:18:15 +0900

    feature_makeDrawingsTagsControllerAndRoute_#17:Added postDel Controller function

--- controllers/drawingsTags/postTagsDel.js
+++ controllers/drawingsTags/postTagsDel.js
@@ -1,5 +1,24 @@
 const { DrawingsTag } = require("../../models");
+const { Drawing } = require("../../models");
 
 module.exports = async (req, res) => {
-  res.send("hello");
+  const Users_id = res.userId;
+  const { Drawings_id } = req.body;
+
+  const checkValid = await Drawing.findOne({
+    where: { Users_id, id: Drawings_id },
+  });
+
+  if (checkValid) {
+    try {
+      await DrawingsTag.destroy({ where: { Drawings_id } });
+      res
+        .status(200)
+        .send({ message: "Your postTags are deleted suceessfully" });
+    } catch (err) {
+      res.status(500).send({ message: `${err.message}` });
+    }
+  } else {
+    res.status(400).send({ message: "You have no right to delete this post" });
+  }
 };
