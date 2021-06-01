5a80a6ff765b1268cddb59b3c77596a81d36f92d
민제현 <alswpgus0@gmail.com>
2021-06-01 09:59:47 +0900

    feature_makeDrawingsTagsControllerAndRoute_#19:Added userGet Controller function

--- controllers/user/userGet.js
+++ controllers/user/userGet.js
@@ -1,3 +1,20 @@
 const { User } = require("../../models");
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const userInfo = await User.findOne({ where: { id } });
+    if (userInfo) {
+      const { nickname, profileImg } = userInfo.dataValues;
+      res.status(200).send({
+        userInfo: { nickname, profileImg },
+        message: "Get User's information successfully",
+      });
+    } else {
+      res.status(400).send({ message: `The user is no exist` });
+    }
+  } catch (err) {
+    res.status(500).send({ message: `${err.message}` });
+  }
+};
