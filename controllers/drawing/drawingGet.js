16d8c28a1786be845ba785df38dcda56c71b42c0
민제현 <alswpgus0@gmail.com>
2021-06-02 19:58:50 +0900

    feature_AddDrawingsControllerAndRoute_#28:Added drainwgGet controller function

--- controllers/drawing/drawingGet.js
+++ controllers/drawing/drawingGet.js
@@ -1,3 +1,20 @@
 const { Drawing } = require("../../models");
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    let postInfo = await Drawing.findOne({ where: { id } });
+    if (postInfo) {
+      postInfo = postInfo.dataValues;
+      res.status(200).send({
+        postInfo,
+        message: "Get the post successfully",
+      });
+    } else {
+      res.status(400).send({ message: `The post is no exist` });
+    }
+  } catch (err) {
+    res.status(500).send({ message: `${err.message}` });
+  }
+};
--- routes/drawing.js
+++ routes/drawing.js
@@ -7,5 +7,7 @@ const checkTokenValid = require("../middlewares/checkTokenValid");
 router.post("/upload", checkTokenValid, getImage, controller.drawingUpload);
 router.patch("/update", checkTokenValid, getImage, controller.drawingUpdate);
 router.delete("/delete", checkTokenValid, controller.drawingDelete);
+router.get("/:id", controller.drawingGet);
+router.get("/getall", controller.drawingGetAll);
 
 module.exports = router;
