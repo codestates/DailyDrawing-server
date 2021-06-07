2b813ad2a30ab08ee3f53b05d770c579ef060c86
민제현 <alswpgus0@gmail.com>
2021-06-07 09:01:24 +0900

    feature_addGetLikeCount_#89:Added likeGet controller and route

--- controllers/like/index.js
+++ controllers/like/index.js
@@ -2,4 +2,5 @@ module.exports = {
   likeUp: require("./likeUp"),
   likeDown: require("./likeDown"),
   likeCount: require("./likeCount"),
+  likeGet: require("./likeGet"),
 };
--- routes/like.js
+++ routes/like.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/like");
 const checkTokenValid = require("../middlewares/checkTokenValid");
 
+router.get("/get", checkTokenValid, controller.likeGet);
 router.post("/up", checkTokenValid, controller.likeUp);
 router.delete("/down", checkTokenValid, controller.likeDown);
 router.get("/count/:id", controller.likeCount);
